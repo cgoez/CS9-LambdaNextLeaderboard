70e9235e83b2ca03193444842248d3cbac8b414f
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-13 18:54:13 -0400

    Created user registration route

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -4,10 +4,46 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
 const User = require("../../models/User");
+const validateRegistration = require("../../validation/registration");
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users route working" }));
+
+// @route   POST api/users/register
+// @desc    Registers new user
+// @access  Public
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegistration(req.body);
+
+  // Validation Check
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ username: req.body.username }).then(user => {
+    if (user) {
+      errors.username = "Username already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(11, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(created => res.json(created))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
