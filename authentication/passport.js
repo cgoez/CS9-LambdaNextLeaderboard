be2022a65d598e364e8369cb19565f0ca2961336
Eric Andrade <eric.andrade@gmail.com>
2018-08-14 10:36:21 -0400

    created passport.js authentication -- initial

---
+++ authentication/passport.js
@@ -0,0 +1,24 @@
+const JWTStragegy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../../config/keys");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtSecret: = keys. jwtSecret;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if(user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
