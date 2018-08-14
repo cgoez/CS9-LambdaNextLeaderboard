56a783d0bc5f77f3475bb8db40b78b353aaaa7a3
Eric Andrade <eric.andrade@gmail.com>
2018-08-14 12:29:15 -0400

    typos

--- backend/authentication/passport.js
+++ backend/authentication/passport.js
@@ -1,4 +1,4 @@
-const JWTStragegy = require("passport-jwt").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
@@ -6,7 +6,7 @@ const keys = require("../config/keys");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.jwtSecret = keys.jwtSecret;
+opts.secretOrKey = keys.jwtSecret;
 
 module.exports = passport => {
   passport.use(
