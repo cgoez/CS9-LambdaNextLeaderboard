fd1a74d60fc79a863da3cf0482fea1b5f66150a5
Eric Andrade <eric.andrade@gmail.com>
2018-08-14 12:10:44 -0400

    fixed typo and moved authentication directory into backend

--- backend/authentication/passport.js
+++ backend/authentication/passport.js
@@ -2,18 +2,18 @@ const JWTStragegy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("../../config/keys");
+const keys = require("../config/keys");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.jwtSecret: = keys. jwtSecret;
+opts.jwtSecret = keys.jwtSecret;
 
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
         .then(user => {
-          if(user) {
+          if (user) {
             return done(null, user);
           }
           return done(null, false);
