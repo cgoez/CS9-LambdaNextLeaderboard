6ef362b8707f3b41a7aedce0f6105feb0eb9f393
Abraham Bueno <abueno@localhost.localdomain>
2018-08-14 00:14:25 -0500

    Added createUser action, and LoginAction in the store. Added the reducers for the createUser action, and LoginAction. Added the components needed for creating a user and login in a user. Worked on the styles for the MenuBar, so it looks more similar to the MVP.

---
+++ leaderboard-frontend/src/actions/actions.js
@@ -0,0 +1,99 @@
+import axios from 'axios';
+
+
+export const CREATE_USER = 'CREATE_USER';
+export const LOGIN_ACTION = 'LOGIN_ACTION';
+export const UPDATE_USER = 'UPDATE_USER';
+export const ERRORS = 'ERRORS';
+
+const ROOT_URL = "Coming SOON";
+
+export const createUserAction = (obj) => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    localStorage.removeItem('notes');
+
+    let username = obj.username;
+    return (dispatch) => {
+        axios.post(`${ROOT_URL}register`, obj)
+            .then(resp => {
+                localStorage.setItem('ID', resp.data._id);
+                localStorage.setItem('username', resp.data.username);
+                dispatch({
+                    type: CREATE_USER,
+                    username: username,
+                    payload: resp
+                });
+            })
+    }
+};
+
+export const loginAction = (obj, history) => {
+    //Need to KNow the token expiration
+    let today = new Date();
+    let time = today.getDate();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    // let expire
+    // if (hours < 12) {
+    //     if (hours === 11) {
+    //         hours = 12;
+    //         if (minutes >= 10) {
+    //             expire = hours + ':' + minutes + 'pm';
+    //         } else {
+    //             expire = hours + ':0' + minutes + 'pm';
+    //         }
+    //     } else {
+    //         hours += 1;
+    //         if (minutes >= 10) {
+    //             expire = hours + ':' + minutes + 'am';
+    //         } else {
+    //             expire = hours + ':0' + minutes + 'am';
+    //         }
+    //     }
+    //     //
+
+    // } else {
+    //     if (hours === 23) {
+    //         hours = 12;
+    //         if (minutes >= 10) {
+    //             expire = hours + ':' + minutes + 'am';
+    //         } else {
+    //             expire = hours + ':0' + minutes + 'am';
+    //         }
+            
+    //     } else {
+    //         hours = hours - 12 + 1;
+    //         if (minutes >= 10) {
+    //             expire = hours + ':' + minutes + 'pm';
+    //         } else {
+    //             expire = hours + ':0' + minutes + 'pm';
+    //         }
+            
+    //     }
+    // }
+    return (dispatch) => {
+        axios.post(`${ROOT_URL}login`, obj)
+            .then(res => {
+                localStorage.setItem('token', res.data.token);
+                localStorage.setItem('username', res.data.username);
+                localStorage.setItem('ID', res.data.Id);
+                // localStorage.setItem('expiration', expire);
+                dispatch({
+                    type: LOGIN_ACTION,
+                    payload: res.data.token,
+                    username: res.data.username,
+                    Id: res.data.Id
+                    // expiration: expire// (Math.floor(Date.now() / 1000) + (60*60))
+                });
+                //Need to get the correct redirect
+                history.push('/')
+            })
+            .catch(() => {
+                localStorage.removeItem('token');
+                localStorage.removeItem('username');
+                dispatch({ type: ERRORS });
+            });
+    }
+};
--- leaderboard-frontend/src/index.js
+++ leaderboard-frontend/src/index.js
@@ -18,7 +18,7 @@ const store = createStore(
 
 function SplitPane(props) {
     return (
-        <div className="SplitPane" style={{ height: '100%', border:"2px solid black" }}>
+        <div className="SplitPane" style={{ height: '100%'}}>
             <div className="SplitPane-left">{props.left}</div>
             <div className="SplitPane-right">{props.right}</div>
         </div>
@@ -26,7 +26,7 @@ function SplitPane(props) {
   }
   function LeftContent(props) {
     return (
-        <div className="LeftContent" style={{ height: '100%', color:"red" }}>
+        <div className="LeftContent" style={{ height: '100%'}}>
             <MenuBar />
         </div>
     );
@@ -43,18 +43,19 @@ function SplitPane(props) {
   
   
   const NoteHome = (props) => {
-    return <div style={{ height: '100%' }}>
-     <SplitPane left={<LeftContent />}  right={<RightContent />} />;
+    return (
+    <div style={{ height: '100%' }}>
+     <SplitPane left={<LeftContent />}  right={<RightContent />} />
      </div>
-  };
+     )
+  }
   
   const RouTING = () => {
     // if (this.props.history.push('/'))
     return (
   
         <div style={{ height: '100%' }}>
-            {/* <h1>Hello</h1> */}
-            <Route path="/" exact component={withRouter(NoteHome )} />
+            <Route path="/" exact component={withRouter(NoteHome)} />
             {/* <Route path="/usercreate" exact component={withRouter(CreateUser)} />
             <Route path="/login" exact component={withRouter(Login)} />
             <Route path="/fetch" exact component={withRouter(Fetch)} />
@@ -66,7 +67,7 @@ function SplitPane(props) {
             <Route exact path="/notes/view/:id" component={withRouter(View)} />
             <Route exact path="/notes/edit/:idE" component={withRouter(Edit)} />
             <Route exact path="/notes/view/delete/:idE" component={withRouter(Delete)} /> */}
-        </div>);
+        </div>)
   }
  
 
