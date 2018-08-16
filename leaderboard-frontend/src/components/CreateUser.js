77e9920a810a82504ae1a8e0ab25b537300ea158
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-16 15:07:53 -0400

    Register is now working.

--- leaderboard-frontend/src/components/CreateUser.js
+++ leaderboard-frontend/src/components/CreateUser.js
@@ -1,124 +1,84 @@
-import React from 'react';
-import { createUserAction } from '../actions';
-import { connect } from 'react-redux';
-const mainStyle = {
-    textAlign: 'center',
-}
+import React from "react";
+import { connect } from "react-redux";
+
+import { createUserAction } from "../actions";
+
 class CreateUser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            usernameconfirm: '',
-            password: '',
-            passwordconfirm: '',
-            message: '',
-            delmessage: "Delete User",
-            sentConfirm: false,
-            sentFailedMessage: ''
-        }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      password2: ""
     };
-    componentDidMount = () => {
+  }
 
-    }
+  handleInput = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-    componentWillUpdate = (nextProps) => {
-        if (this.state.sentConfirm === true) {
-            
-            // console.log("Props for check update", this.props.updateCheck, nextProps.updateCheck);
-            // console.log("message and sentconfirm", this.state.message, this.state.sentConfirm);
-        } 
-        if (this.state.sentConfirm === true && nextProps.updateCheck === true) {
-            // console.log("Redirect")
-            this.setState({sentConfirm : false});
-            this.props.history.push('/login')
-        
-    }
-    if (this.state.sentConfirm === true && this.props.updateCheck === false) { 
-        this.setState({sentFailedMessage: "Update failed because the username has already been taken, please try a different username"})
+  handleSubmit = () => {
+    this.props.createUserAction(this.state);
+    this.setState({ password: "", password2: "" });
+  };
 
-        this.setState({sentConfirm : false});
-    }
-    }
-    newCredentials = () => {
-        let username = this.state.username;
-        let password = this.state.password;
-        const newObject = {
-            username: username.toLowerCase().toString(),
-            password: password,
-            password2: this.state.passwordconfirm // The server is setup to get 2 passwords for registry.
-            //It should only need one password, it's better to check in the client if they match.
-        };
-        console.log(newObject)
-        this.setState({ username: '',usernameconfirm: '' , password: '', passwordconfirm: '' });
-        this.setState({ sentConfirm: true });
-        this.props.createUserAction(newObject);
-    }
-    // checkCredentials = () => {
-    //     if (this.state.password === this.state.passwordconfirm && this.state.username === this.state.usernameconfirm && this.state.username !== '' && this.state.password !== '') {
-    //         this.newCredentials();
-    //     } else {
-    //         this.setState({ username: '', password: '', passwordconfirm: '', username: '', usernameconfirm: '', message: "Update Failed, due to mismatch password or username, try again" });
-    //     }
-    // }
-    handleInput = (e) => {
-        e.preventDefault();
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-    render() {
-        return (
-            <div style={mainStyle} >
-                <h3>CreateUser Component</h3>
-                {this.state.message === '' ? <div></div> : <h3>{this.props.error}</h3>}
-                {(this.props.updateCheck === false && this.state.sentConfirm === true) ? <div> <h3>Update failed because the username has already been taken, please try a different username</h3> </div>:<div></div>  }
-                <form>
-                    <input
-                        type="text"
-                        placeholder="Enter new username"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleInput}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Confirm new username"
-                        name="usernameconfirm"
-                        value={this.state.usernameconfirm}
-                        onChange={this.handleInput}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Enter password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleInput}
-                    />
-                    <input
-                        type="password"
-                        name="passwordconfirm"
-                        placeholder="Confirm Password"
-                        value={this.state.passwordconfirm}
-                        onChange={this.handleInput}
-                    />
-                </form>
-                <div>
+  render() {
+    return (
+      <div className="rgstr__container">
+        <h2 className="rgstr__header">ReGiStEr</h2>
+        {/* USERNAME */}
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInput}
+          className="rgstr__input"
+        />
 
-                    <button onClick={this.newCredentials} >Submit </button>
-                </div>
-            </div>
-        )
-    }
+        {/* PASSWORD */}
+        <input
+          type="text"
+          placeholder="Password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInput}
+          className="rgstr__input"
+        />
 
+        {/* PASSWORD CONFIRMATION */}
+        <input
+          type="text"
+          placeholder="Confirm Password"
+          name="password2"
+          value={this.state.password2}
+          onChange={this.handleInput}
+          className="rgstr__input"
+        />
+
+        {/* BUTTON */}
+        <button onClick={this.handleSubmit} className="rgstr__btn">
+          Sign Up
+        </button>
+
+        {/* SIGN IN */}
+        <a href="/login" className="rgstr__login">
+          Already have an account? Sign in
+        </a>
+      </div>
+    );
+  }
 }
+
 const mapStateToProps = state => {
-    return {
-        user: state.user,
-        updateReceived: state.updateReceived,
-        updateCheck: state.updateCheck,
-        error: state.error
-    }
-}
+  return {
+    error: state.error
+  };
+};
 
-export default connect(mapStateToProps, { createUserAction })(CreateUser)
+export default connect(
+  mapStateToProps,
+  { createUserAction }
+)(CreateUser);
