775ef5620d579413d32f36e55c53d3fe4ed7514e
cgoez <goez2001@gmail.com>
2018-08-15 18:22:07 -0700

    Set Github API fetching, store local

--- backend/data/githubData.js
+++ backend/data/githubData.js
@@ -1,11 +1,16 @@
 const axios = require("axios");
 const _ = require("lodash");
+const router = require("express").Router();
 
 // Fetch # commits per user
 //
 async function fetchGithubData(githubHandle) {
+  let authStr = `Bearer ${OAUTH_TOKEN}`;
+
   return await axios
-    .get(`https://api.github.com/users/${githubHandle}/events/public`)
+    .get(`https://api.github.com/users/cgoez/events/public`, {
+      Authorization: authStr
+    })
     .then(res => {
       const data = res.data;
 
@@ -15,6 +20,23 @@ async function fetchGithubData(githubHandle) {
         .flatMap()
         .map(item => item.message)
         .value();
-    });
+    })
+    .catch(err => console.log(err));
 }
 
+// Int storage var
+let storage;
+
+// Fetch Github data every 10s
+setInterval(async () => {
+  console.log("Fetching github data");
+  storage = await fetchGithubData();
+  console.log("Finished");
+}, 10000);
+
+//
+router.get("/", async (req, res) => {
+  res.send(storage);
+});
+
+module.exports = router;
